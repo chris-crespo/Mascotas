e5a99ae730f18011476dbb863bfe2c3cf27cb30c
krs98 <christian.crespoo98@gmail.com>
2022-03-19 17:47:36 +0100

    Init repos

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,8 +1,13 @@
 ﻿using Mascotas;
 using Mascotas.UI.Console;
+using Mascotas.Data;
+
+var memberRepo = new MemberRepo();
+var petRepo    = new PetRepo();
+var specieRepo = new SpecieRepo();
 
 var view = new View();
-var system = new Manager();
+var system = new Manager(memberRepo, petRepo, specieRepo);
 var controller = new Controller(view, system);
 
 #if !check
