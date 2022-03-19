4cec3b00bdd902e4b8c36d99847cd414d8d8fa62
krs98 <christian.crespoo98@gmail.com>
2022-03-19 16:29:54 +0100

    Implement models

--- src/Models/Models.cs
+++ src/Models/Models.cs
@@ -6,33 +6,58 @@ public class Member
     public string Name   { get; }
     public char   Gender { get; }
 
-    public Member Parse(string str) 
+    public Member(Guid id, string name, char gender) 
     {
-        var fields = line.Split(",");
-        return new Member {}; // TODO
+        ID     = id;
+        Name   = name;
+        Gender = gender;
     }
 
-    public string ToCsv() => ""; //TODO
-    public override ToString() => ""; //TODO
+    public static Member Parse(string str) 
+    {
+        var fields = str.Split(",");
+        return new Member(
+            id:     Guid.Parse(fields[0]),
+            name:   fields[1],
+            gender: fields[2][0]
+        );
+    }
+
+    public override string ToString() 
+        => $"{ID},{Name},{Gender}";
 }
 
 public class Pet
 {
-    public string   Name      { get; set; }
-    public char     Gender    { get; set; }
-    public Specie   Specie    { get; set; }
-    public DateTime Birthdate { get; set; }
-    public Member   Member    { get; set; }
+    public string   Name      { get; }
+    public char     Gender    { get; }
+    public Guid     SpecieID  { get; }
+    public DateTime Birthdate { get; }
+    public Guid     MemberID  { get; }
 
-    public Pet Parse(string str)
+    public Pet(string name, char gender, Guid specieID, DateTime birthdate, Guid memberID)
     {
-        return new Pet {}; // TODO;
+        Name      = name;
+        Gender    = gender;
+        SpecieID  = specieID;
+        Birthdate = birthdate;
+        MemberID  = memberID;
     }
 
-    public strin ToCsv() => ""; // TODO
+    public static Pet Parse(string str)
+    {
+        var fields = str.Split(",");
+        return new Pet(
+            name:      fields[0],
+            gender:    fields[1][0],
+            specieID:  Guid.Parse(fields[2]),
+            birthdate: DateTime.Parse(fields[3]),
+            memberID:  Guid.Parse(fields[4])
+        );
+    }
 
-    public override string ToString()
-        => $"{Name},{Gender},{Specie},{Birthdate},{Member.Name}";
+    public override string ToString() 
+        => $"{Name},{Gender},{SpecieID},{Birthdate},{MemberID}";
 }
 
 public class Specie
@@ -40,5 +65,21 @@ public class Specie
     public Guid   ID   { get; }
     public string Name { get; }
 
-    public override string ToString() => Name;
+    public Specie(Guid id, string name) 
+    {
+        ID   = id;
+        Name = name;
+    }
+
+    public static Specie Parse(string str)
+    {
+        var fields = str.Split(",");
+        return new Specie(
+            id:   Guid.Parse(fields[0]),
+            name: fields[1]
+        );
+    }
+
+    public override string ToString() 
+        => $"{ID},{Name}";
 }
