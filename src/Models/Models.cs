b152f18e1a1b1166a50fa2c734e1791114ea7e28
krs98 <christian.crespoo98@gmail.com>
2022-03-19 17:45:44 +0100

    Change pet representation

--- src/Models/Models.cs
+++ src/Models/Models.cs
@@ -29,14 +29,16 @@ public class Member
 
 public class Pet
 {
+    public Guid     ID        { get; }
     public string   Name      { get; }
     public char     Gender    { get; }
     public Guid     SpecieID  { get; }
     public DateTime Birthdate { get; }
-    public Guid     MemberID  { get; }
+    public Guid?    MemberID  { get; }
 
-    public Pet(string name, char gender, Guid specieID, DateTime birthdate, Guid memberID)
+    public Pet(Guid id, string name, char gender, Guid specieID, DateTime birthdate, Guid? memberID)
     {
+        ID        = id;
         Name      = name;
         Gender    = gender;
         SpecieID  = specieID;
@@ -48,16 +50,17 @@ public class Pet
     {
         var fields = str.Split(",");
         return new Pet(
-            name:      fields[0],
-            gender:    fields[1][0],
-            specieID:  Guid.Parse(fields[2]),
-            birthdate: DateTime.Parse(fields[3]),
-            memberID:  Guid.Parse(fields[4])
+            id:        Guid.Parse(fields[0]),
+            name:      fields[1],
+            gender:    fields[2][0],
+            specieID:  Guid.Parse(fields[3]),
+            birthdate: DateTime.Parse(fields[4]),
+            memberID:  Guid.Parse(fields[5])
         );
     }
 
     public override string ToString() 
-        => $"{Name},{Gender},{SpecieID},{Birthdate},{MemberID}";
+        => $"{ID},{Name},{Gender},{SpecieID},{Birthdate},{MemberID}";
 }
 
 public class Specie
